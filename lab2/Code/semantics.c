2b6aa177bba601481bf4a0b6e5587c18c0f29bf2
Zhuo Chen <ouhznehc@foxmail.com>
2024-04-12 17:44:26 +0800

    Auto commit message: { Compile code at 2024-04-12 17:44:26 }

--- lab2/Code/semantics.c
+++ lab2/Code/semantics.c
@@ -140,6 +140,7 @@ bool typecmp_name(type_t* t1, type_t* t2) {
             function:
                 if (strcmp(t1->function.name, t2->function.name)) return false;
                 if (t1->function.argc != t2->function.argc) return false;
+                if (!typecmp_name(t1->function.ret, t2->function.ret)) return false;
                 cur1 = t1->function.argv;
                 cur2 = t2->function.argv;
                 while (cur1 != NULL && cur2 != NULL){
@@ -179,6 +180,7 @@ bool typecmp_structure(type_t* t1, type_t* t2) {
             function:
                 if (strcmp(t1->function.name, t2->function.name)) return false;
                 if (t1->function.argc != t2->function.argc) return false;
+                 if (!typecmp_name(t1->function.ret, t2->function.ret)) return false;
                 cur1 = t1->function.argv;
                 cur2 = t2->function.argv;
                 while (cur1 != NULL && cur2 != NULL){
