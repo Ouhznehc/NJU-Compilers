a90f1d0eb014a18813163e5a0880fa41c9136ad2
Zhuo Chen <ouhznehc@foxmail.com>
2024-04-12 16:14:20 +0800

    Auto commit message: { Compile code at 2024-04-12 16:14:20 }

--- lab2/Code/semantics.c
+++ lab2/Code/semantics.c
@@ -485,7 +485,7 @@ item_t* VarDec(syntax_t* node, type_t* specifier) { printf("This is line number 
 
     // VarDec -> VarDec LB INT RB
     else if (symcmp(childs[0], "VarDec")) {
-        printf("%d\n", childs[2]->token.value.ival);
+        printf("%p %d\n", specifier, childs[2]->token.value.ival);
         type_t* array = new_type(Array, specifier, childs[2]->token.value.ival);
         return VarDec(childs[0], array);
     }
