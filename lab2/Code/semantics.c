ab61f4ef0635382cab32815087c66437e7ec8b34
Zhuo Chen <ouhznehc@foxmail.com>
2024-04-12 15:46:12 +0800

    Auto commit message: { Compile code at 2024-04-12 15:46:12 }

--- lab2/Code/semantics.c
+++ lab2/Code/semantics.c
@@ -905,11 +905,11 @@ void Args(syntax_t* node, item_t* func) {printf("This is line number %d.\n", __L
     syntax_t* cur_arg = node;
     while (cur_arg && func_arg) {
         type_t* func_type = func_arg->type;
-        type_t* cur_type = Exp(cur_arg->symbol.child[0]);printf("++================\n");
+        type_t* cur_type = Exp(cur_arg->symbol.child[0]);
         if (!typecmp(func_type, cur_type)) {
                 semantic_error(MISMATCHED_FUNC_ARG, cur_arg->lineno, ""); 
                 return;
-        }
+        }printf("++================\n");
         func_arg = func_arg->next;
         cur_arg = cur_arg->symbol.child[2];
     }
