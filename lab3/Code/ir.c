69dfb64cce8e4c5c071da00e36a1f2865c964936
Zhuo Chen <ouhznehc@foxmail.com>
2024-04-27 17:29:50 +0800

    Auto commit message: { Compile code at 2024-04-27 17:29:50 }

--- lab3/Code/ir.c
+++ lab3/Code/ir.c
@@ -1,5 +1,66 @@
 #include "ir.h"
 
+/* High-level Definitions */
+void translate_Program(syntax_t* node); 
+void translate_ExtDefList(syntax_t* node); 
+void translate_ExtDef(syntax_t* node); 
+void translate_ExtDecList(syntax_t* node); 
+
+/* Specifiers */
+void translate_Specifier(syntax_t* node); 
+void translate_StructSpecifier(syntax_t* node); 
+void translate_OptTag(syntax_t* node); 
+void translate_Tag(syntax_t* node); 
+
+/* Declarators */
+void translate_VarDec(syntax_t* node); 
+void translate_FunDec(syntax_t* node); 
+void translate_VarList(syntax_t* node); 
+void translate_ParamDec(syntax_t* node); 
+
+/* Statements */
+void translate_CompSt(syntax_t* node); 
+void translate_StmtList(syntax_t* node); 
+void translate_Stmt(syntax_t* node);
+
+/* Local Definitions */
+void translate_DefList(syntax_t* node); 
+void translate_Def(syntax_t* node); 
+void translate_Dec(syntax_t* node); 
+void translate_DecList(syntax_t* node); 
+
+/* Expressions */
+void translate_Exp(syntax_t* node); 
+void translate_Args(syntax_t* node); 
+
+void* malloc_safe(int size) {
+    void* ret = malloc(size);
+    memset(ret, 0, size);
+    return ret;
+}
+
+arg_t* new_arg(int kind, char* name, int cons) {
+    arg_t* ret = malloc_safe(sizeof(arg_t));
+    ret->kind = kind;
+    switch (kind) {
+        case ArgVar:
+        case ArgTmp:
+        case ArgImm:
+        case ArgLabel:
+        case ArgSize:
+            ret->cons = cons;
+        case ArgFunc:
+        case ArgRelop:
+        case ArgRef:
+        case ArgDeref:
+            strcpy(ret->name, name);
+        default: assert(0);
+    }
+    return ret;
+}
+
+
+
 /* 
 Program:    
     | ExtDefList
@@ -205,14 +266,62 @@ Args:
 */
 void translate_Args(syntax_t* node) { 
     return;
-} 
-
-void display_ir_code(FILE* ir) {
-    return;
 }
+
+char* display_arg(arg_t* arg) {
+    if (arg == NULL) return NULL;
+    char* ret = malloc_safe(sizeof(char) * 64);
+    switch (arg->kind) {
+        case ArgVar:        sprintf(ret, "v%d", arg->cons); break;
+        case ArgTmp:        sprintf(ret, "t%d", arg->cons); break;
+        case ArgLabel:      sprintf(ret, "label%d", arg->cons); break;
+        case ArgFunc:       sprintf(ret, "%s", arg->name); break;
+        case ArgRelop:      sprintf(ret, "%s", arg->name); break;
+        case ArgSize:       sprintf(ret, "%d", arg->cons); break;
+        case ArgRef:        sprintf(ret, "&%s", arg->name); break;
+        case ArgDeref:      sprintf(ret, "*%s", arg->name); break;
+        default: assert(0);
+    }
+    return ret;
+}
+char* display_ic(ic_t* ic) {
+    char* ret = malloc_safe(sizeof(char) * 64);
+    char* result = display_arg(ic->result);
+    char* arg1 = display_arg(ic->arg1);
+    char* arg2 = display_arg(ic->arg2);
+    char* relop = display_arg(ic->relop);
+    switch (ic->op) {
+        case IcLabel:       sprintf(ret, "LABEL %s :\n", result);
+        case IcFunc:        sprintf(ret, "FUNCTION %s :\n", result);
+        case IcAssign:      sprintf(ret, "%s := %s\n", result, arg1);
+        case IcAdd:         sprintf(ret, "%s := %s + %s\n", result, arg1, arg2);
+        case IcSub:         sprintf(ret, "%s := %s - %s\n", result, arg1, arg2);
+        case IcMul:         sprintf(ret, "%s := %s * %s\n", result, arg1, arg2);
+        case IcDiv:         sprintf(ret, "%s := %s / %s\n", result, arg1, arg2);
+        case IcGoto:        sprintf(ret, "GOTO %s\n", result);
+        case IcBranch:      sprintf(ret, "IF %s %s %s GOTO %s\n", arg1, relop, arg2, result);
+        case IcReturn:      sprintf(ret, "RETURN %s\n", result);
+        case IcArg:         sprintf(ret, "ARG %s\n", result);
+        case IcCall:        sprintf(ret, "%s := CALL %s\n", result, arg1);
+        case IcParam:       sprintf(ret, "PARAM %s\n", result);
+        case IcRead:        sprintf(ret, "READ %s\n", result);
+        case IcWrite:       sprintf(ret, "WRITE %s\n", result);
+        default: assert(0);
+    }
+    return ret;
+}
+
+void display_ir(FILE* ir) {
+    ir_t* cur = ir_head;
+    while (cur != NULL) {
+        fprintf(ir, "%s", display_ic(cur->code));
+        cur = cur->next;
+    }
+}
+
 
 void generate_ir(syntax_t* root, FILE* ir) {
     translate_Program(root);
-    display_ir_code(ir);
+    display_ir(ir);
     return;
 }
--- lab3/Code/ir.h
+++ lab3/Code/ir.h
@@ -3,5 +3,53 @@
 #include "grammar.h"
 #include "semantics.h"
 
+typedef struct arg_t arg_t;
+typedef struct ic_t ic_t;
+typedef struct ir_t ir_t;
+
+typedef enum {
+    ArgVar, ArgTmp, ArgImm,
+    ArgLabel, ArgFunc, ArgRelop, ArgSize,
+    ArgRef, ArgDeref
+} ArgKind;
+typedef enum {
+    IcLabel, IcFunc, IcAssign, 
+    IcAdd, IcSub, IcMul, IcDiv, 
+    IcGoto, IcBranch, IcReturn,
+    IcArg, IcCall, IcParam, 
+    IcRead, IcWrite
+} InterCodeKind;
+
+struct arg_t {
+    ArgKind kind;
+    union {
+        int cons; // for const value like: label1, #5, DEC a 100, etc.
+        char name[64]; // for variable name like: t1, v10, etc.
+    };
+};
+
+/// struct of intercode, in detail:
+// Assignment: result := arg1
+// binary op: result := arg1 op arg2
+// single op: result := op arg1
+// GOTO ARG PARAM FUNCTION LABEL RETURN: result
+// CALL: result := CALL arg1
+// goto: IF arg1 relop arg2 GOTO result
+// declaration: DEC result arg1
+struct ic_t {
+    InterCodeKind op;
+    arg_t *result, *arg1, *arg2, *relop; 
+};
+
+// List of intercodes
+struct ir_t {
+    ic_t* code;
+    ir_t* prev, *next;
+};
+
+ir_t *ir_head, *ir_tail;
+int var_no = 0, tmp_no = 0, label_no = 0;
+
 void generate_ir(syntax_t* root, FILE* ir);
+
 #endif
