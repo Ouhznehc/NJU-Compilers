898bf680731fe2f6e59caef8830d6b6eae53c0c8
Zhuo Chen <ouhznehc@foxmail.com>
2024-05-25 00:18:54 +0800

    Auto commit message: { Compile code at 2024-05-25 00:18:54 }

--- lab4/Code/mips.h
+++ lab4/Code/mips.h
@@ -7,6 +7,14 @@ extern ir_t *ir_head, *ir_tail;
 extern var_t* varlist;
 extern int tmp_no;
 
+typedef struct arglist_t arglist_t;
+struct arglist_t {
+    arg_t* arg;
+    arglist_t* next;
+};
+
+arglist_t* arglist;
+
 void generate_asm(FILE* fp);
 
 #endif
